9b294eff056c918281cbb37039fc8a2d7dc9c14c
David Gretz <dg@buckeyeguy.com>
2016-10-26 17:39:25 +0700

    P3-19 commit

--- LastEmo/ViewController.swift
+++ LastEmo/ViewController.swift
@@ -8,13 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var coolTableview: UITableView!
+    
+    var emojis = ["😀 The Happy One","😇 The Angel","🤔 Mr. Thoughtful","😛 the Taunt","👻 Boo Boo","🍎 For the Teacher","🐧 For Elijah","🐝Bee Smart"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        coolTableview.dataSource=self
+        coolTableview.delegate=self
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return emojis.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell=UITableViewCell()
+        cell.textLabel?.text = emojis[indexPath.row]
+        return cell
+    }
+
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
